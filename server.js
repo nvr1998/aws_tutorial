bc62a08f26bbaa4002ab21bfc7c57a69b00fde92
nvr1998 <gnickyraj@gmail.com>
2024-07-14 12:16:29 +0530

    minor

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ import server from "./app.js";
 import { PORT } from "./solution.js";
 
 server.listen(PORT, () => {
-  console.log(`server is running on ${process.env.PORT}`);
+  console.log(`server is running on ${PORT}`);
 });
